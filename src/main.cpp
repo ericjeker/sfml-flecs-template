497c95ad06d9607dd763b75841a695938d9ba21c
Eric Jeker <eric@wowww.ch>
2025-09-13 09:08:08 +0200

    feat: add flecs

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
+#include <flecs.h>
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Angle.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/WindowEnums.hpp>
+#include <flecs/addons/cpp/mixins/pipeline/decl.hpp>
+
+struct CircleRenderable {
+  sf::CircleShape shape = sf::CircleShape(100.f, 3);
+};
+
+struct Transform {
+  sf::Vector2f position;
+  sf::Vector2f scale = {1.f, 1.f};
+  float rotation = 0.f;
+};
 
 int main() {
   sf::ContextSettings settings;
@@ -10,13 +25,28 @@ int main() {
 
   auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project", sf::Style::None,
                                  sf::State::Fullscreen, settings);
-  window.setFramerateLimit(60);
+  window.setFramerateLimit(144);
+
+  // the unique flecs world
+  const auto world = flecs::world();
 
   // circle with 100-pixel radius, green fill
-  sf::CircleShape circle(100.f, 3);
-  circle.setFillColor(sf::Color::Green);
-  circle.setOrigin({100.f, 100.f});
-  circle.setPosition({1920 / 2.f, 1080 / 2.f});
+  auto triangle = world.entity().set<CircleRenderable>({}).set<Transform>({{1920 / 2.f, 1080 / 2.f}});
+  auto& shape = triangle.get_mut<CircleRenderable>().shape;
+  shape.setFillColor(sf::Color::Green);
+  shape.setOrigin({100.f, 100.f});
+
+  // add our systems to the world
+  world.system<Transform>().each(
+      [](const flecs::iter& it, size_t, Transform& t) { t.rotation += 90.f * it.delta_time(); });
+
+  world.system<CircleRenderable, const Transform>()
+      .kind(flecs::OnStore)
+      .each([&window](CircleRenderable c, const Transform t) {
+        c.shape.setRotation(sf::degrees(t.rotation));
+        c.shape.setPosition(t.position);
+        window.draw(c.shape);
+      });
 
   sf::Clock clock;
   while (window.isOpen()) {
@@ -32,10 +62,8 @@ int main() {
       }
     }
 
-    circle.setRotation(circle.getRotation() + sf::degrees(100.f * elapsed));
-
     window.clear();
-    window.draw(circle);
+    world.progress(elapsed);
     window.display();
   }
 
